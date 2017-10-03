20bb90acbe97283b6d46a43b41669c3386001f20
Jrawat <plattinum_09@hotmail.com>
2017-10-04 02:46:37 +0700

    newloop

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const ScoreLiveController = require('../controllers/scoreliveController');
-const ScoreNonLiveController = require('../controllers/scorenonliveController');
-const notiController = require('../controllers/notiController');
+// const ScoreLiveController = require('../controllers/scoreliveController');
+// const ScoreNonLiveController = require('../controllers/scorenonliveController');
+// const notiController = require('../controllers/notiController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	notiController.findTeamDistinct({}).then((result) => {
-    	res.render('index.ejs', { match:result });
-  	});
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+// 	notiController.findTeamDistinct({}).then((result) => {
+//     	res.render('index.ejs', { match:result });
+//   	});
+// });
 
-router.post('/matchesDetail/:date/:view', notiController.getAllMatches);
-router.post('/matches/:id', notiController.getMatches);
-router.get('/matches/:id/detail', ScoreNonLiveController.matcheDetail);
+// router.post('/matchesDetail/:date/:view', notiController.getAllMatches);
+// router.post('/matches/:id', notiController.getMatches);
+// router.get('/matches/:id/detail', ScoreNonLiveController.matcheDetail);
 
 module.exports = router;
